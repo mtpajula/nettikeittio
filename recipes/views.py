08090cb3fa0d7d458210df784ee7afbdc3caa48b
Mikko Pajula <mtpajula@gmail.com>
2011-04-02 13:56:28 +0300

    urlseja viewejä

--- recipes/views.py
+++ recipes/views.py
@@ -1,23 +1,58 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpRequest
-from recipes.models import Recipe
+from recipes.models import *
 
 # Simulate slow response from server with time.sleep(2)
 # import time
 
-def base(request):
+def main_page(request):
     return render_to_response('recipes/contentpage.html', { })
 
-def list(request):
+def list_recipes(request):
     recipe_list = Recipe.objects.all().order_by('name')
     
     if HttpRequest.is_ajax(request):
         return render_to_response('recipes/contentpage/recipe_list.html', {'recipe_list': recipe_list, })
     return render_to_response('recipes/contentpage/list.html', {'recipe_list': recipe_list, })
 
-def detail(request, recipe_id):
+def search(request):
+    return render_to_response('recipes/contentpage.html', { })
+
+def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     return render_to_response('recipes/contentpage/detail.html', { 'recipe': recipe })
 
-def fullpage(request):
+def active(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render_to_response('recipes/fullpage.html', { 'recipe': recipe })
+
+def edit_recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render_to_response('recipes/contentpage/detail.html', { 'recipe': recipe })
+
+def new_recipe(request):
     return render_to_response('recipes/fullpage.html', { })
+
+def user_detail(request, user_id):
+    user = get_object_or_404(UserProfile, pk=user_id)
+    return render_to_response('recipes/contentpage/user.html', { 'user': user })
+
+def new_user(request):
+    return render_to_response('recipes/contentpage/user.html', { })
+
+def edit_user(request, user_id):
+    user = get_object_or_404(UserProfile, pk=user_id)
+    return render_to_response('recipes/contentpage/user.html', { 'user': user })
+
+def nk_login(request):
+    return render_to_response('recipes/contentpage.html', { })
+
+def nk_logout(request):
+    return render_to_response('recipes/contentpage.html', { })
+
+def register(request):
+    return render_to_response('recipes/contentpage.html', { })
+
+def nk_help(request):
+    return render_to_response('recipes/contentpage.html', { })
+
--- urls.py
+++ urls.py
@@ -1,22 +1,31 @@
 from django.conf.urls.defaults import *
-from django.conf import settings
+import settings
+from recipes.views import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^/*$', 'recipes.views.base'),
-    (r'^recipes/$', 'recipes.views.list'),
-    (r'^fullpage/$', 'recipes.views.fullpage'),
-    (r'^recipes/(?P<recipe_id>\d+)/$', 'recipes.views.detail'),
-    (r'^css/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.CSS_ROOT
-    }),
-    (r'^js/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.JS_ROOT
-    }),
-
+    (r'^/*$'                               , main_page),
+    url(r'^recipes/$'                      , list_recipes, name='list_recipes'),
+    url(r'^search/$'                       , search, name='search'),
+    url(r'^recipes/(?P<recipe_id>\d+)/$'   , recipe_detail, name='detail'),
+    url(r'^active/(?P<recipe_id>\d+)/$'    , active, name='active'),
+    url(r'^editrecipe/(?P<recipe_id>\d+)/$', edit_recipe, name='edit_recipe'),
+    url(r'^newrecipe/$'                    , new_recipe, name='new_recipe'),
+    url(r'^user/(?P<user_id>\d+)/$'        , user_detail, name='user_page'),
+    url(r'^newuser/$'                      , new_user, name='new_user'),
+    url(r'^edituser/(?P<user_id>\d+)/$'    , edit_user, name='edit_user'),
+    url(r'^login/$'                        , nk_login, name='nk_login'),
+    url(r'^logout/$'                       , nk_logout, name='nk_logout'),
+    url(r'^register/$'                     , register, name='register'),
+    url(r'^help/$'                         , nk_help, name='nk_help'),
+    (r'^css/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': settings.CSS_ROOT}),
+    (r'^js/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': settings.JS_ROOT}),
+    
 
     # Example:
     # (r'^nettikeittio/', include('nettikeittio.foo.urls')),
@@ -26,5 +35,5 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    #(r'^admin/', include(admin.site.urls)),
 )
